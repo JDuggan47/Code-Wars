7cff1a441e6a37a5a6bf7210a649b190414e5d81
John Duggan <JLDuggan87@gmail.com>
2017-07-19 16:31:59 -0400

    another solution

---
+++ Week of July 16/Replace With Alphabet Position/Solution.js
@@ -0,0 +1,43 @@
+let alphabetPosition = (text) => {
+  let string = text.toLowerCase();
+  let results = '';
+
+  let alphabet = {
+    a: '1',
+    b: '2',
+    c: '3',
+    d: '4',
+    e: '5',
+    f: '6',
+    g: '7',
+    h: '8',
+    i: '9',
+    j: '10',
+    k: '11',
+    l: '12',
+    m: '13',
+    n: '14',
+    o: '15',
+    p: '16',
+    q: '17',
+    r: '18',
+    s: '19',
+    t: '20',
+    u: '21',
+    v: '22',
+    w: '23',
+    x: '24',
+    y: '25',
+    z: '26'
+  }
+
+  for(i = 0; i < string.length; i++) {
+    if(alphabet[string[i]] !== undefined) {
+      let number = " " +  alphabet[string[i]]
+      if(number > 0 && number <= 26) {
+        results += number;
+      }
+    }
+  }
+  return results.substr(1);
+}
